01a76a5528b835fcb64dd8f3f38a861166ddd1db
faizsahmed <faizsahmed@gmail.com>
2020-10-25 22:20:48 +0800

    1.0 - This is the Le Poo 0.1 Alpha update. 2.1 - The greatest poos in history. 2.2 - Soon, the Burp updates will be released. 2.3 - The Burp upates must be postponed due the reign of the Poo AI.       Fixed an issue where the User Input did not exist and did not exist in the 'pooAI' string. 2.4 - Banished the Poo AI (hopefully, permanently) to the Intranet Graveyard. Hashed out the Poo AI's virus.
    MINECRAFF

--- two/two-four.py
+++ two/two-four.py
@@ -1,2 +1,9 @@
-personnFurstNamm =
-print(personNamm)
+personnFurstNamm = "burp"
+personnLastNamm = "god"
+personnFullNamm = f"{personnFurstNamm} {personnLastNamm}"
+print(personnFullNamm.upper())
+print(personnFullNamm.lower())
+print(personnFullNamm.title())
+# print("THE BURP GOD SHALL NOT EXXIST, FOR HE IS NOT A GOD, BUT A DoG!!!!!!!!!!!!!")
+# while personnFullNamm != "burpDoG":
+#     print("ritual not found for the birth of the Burp De--------------")
