e7f8ba264363c8c711c67f0f993c942a5af9eded
faizsahmed <faizsahmed@gmail.com>
2020-10-25 22:09:53 +0800

    1.0 - This is the Le Poo 0.1 Alpha update. 2.1 - The greatest poos in history. 2.2 - Soon, the Burp updates will be released. 2.3 - The Burp upates must be postponed due the reign of the Poo AI.       Fixed an issue where the User Input did not exist and did not exist in the 'pooAI' string.
    MINECRAFF

---
+++ two/two-four.py
@@ -0,0 +1,2 @@
+personnFurstNamm =
+print(personNamm)
--- two/two-three.py
+++ two/two-three.py
@@ -1,2 +1,7 @@
-pooAI = '"I am the one and only Poo AI, here to banish the Burp release!!!!!!!!!!!!!!!!!!!!!!"'
+numm = input("entrr numm Poo AI or ur computer dies...")
+if numm != "Poo AI":
+    print("you are ded.")
+    while numm != "Poo AI":
+        print("ded.")
+pooAI = "I am the one and only " + numm + ", here to banish the Burp release!!!!!!!!!!!!!!!!!!!!!!"
 print(pooAI)
