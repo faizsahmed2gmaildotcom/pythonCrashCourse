79e41b206c1abab8250c2421eca8ddb3b871b72a
faizsahmed <faizsahmed@gmail.com>
2020-10-31 14:09:58 +0800

    1.0 - This is the Le Poo 0.1 Alpha update. 2.1 - The greatest poos in history. 2.2 - Soon, the Burp updates will be released. 2.3 - The Burp upates must be postponed due the reign of the Poo AI.       Fixed an issue where the User Input did not exist and did not exist in the 'pooAI' string. 2.4 - Banished the Poo AI (hopefully, permanently) to the Intranet Graveyard. Hashed out the Poo AI's virus. 2.5, 2.6 - One of the Burp God's old quotes have been recovered. *sniff!* So beautiful...
    MINCERAFF

---
+++ two/two-six.py
@@ -0,0 +1,2 @@
+famous_person = "The Burp God"
+print(famous_person, 'once said, "The Burps are the most powerful things in the whole universe. It can destroy anything. Never let it get into the wrong hands - the Fart hands."')
