724de5f5e134caf65b1825091c14334f4eb2c4ee
faizsahmed <faizsahmed@gmail.com>
2020-12-11 13:38:52 +0800

    These annotations... could it be? Our helper, The Burp God! Yes! We can solve these problems together, defeating it!...? It? Him? Her? Who knows...

--- two/two-eight.py
+++ two/two-eight.py
@@ -1,4 +1,4 @@
-print(4+4)
+print(4+4) # adds four with four
 print(10-2)
 print(2*4)
-print(72/9)
+print(72/9) # divides 72 by nine
--- two/two-nine.py
+++ two/two-nine.py
@@ -1,2 +1,2 @@
 favNum = "7"
-print("My favourite number is " + favNum)
+print("My favourite number is " + favNum) # Embeds the string variable 'favNum' in a scentence.
