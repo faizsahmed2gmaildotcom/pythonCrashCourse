76c77da2bb955a57743f47c6ffcd76a22b9c7aa2
faizsahmed <faizsahmed@gmail.com>
2021-06-06 14:03:19 +0800

    3.9 - Let's see what we did in our recent update, 3.4. Hmm... inviting '???' to dinner? Who... are they really 'Gods' of the toilet?

---
+++ three/three-nine.py
@@ -0,0 +1,6 @@
+invDinner = ['Burp', 'Tyee', 'Puu']
+dinNum = 0
+for dinNum in range (3):
+    print('Dear ' + invDinner[dinNum] + ', please come for dinner.')
+    dinNum += 1
+print('I am inviting ' + str(len(invDinner)) + ' ??? to dinner.')
