46f9d7e536237a6c7de269918dc9be7e086b4c58
faizs <faizsahmed@gmail.com>
2021-07-08 14:28:09 +0800

    The Math Blurfer showed up again, but we were able to rid him.

---
+++ three/three-ten.py
@@ -0,0 +1,19 @@
+lords = ['Burp', 'Pee', 'Fart', 'Puu']
+input(lords)
+input('INCOMING LORD...')
+
+lords.append('Math Blurfer')
+input(lords)
+input("Huh??? No! Get rid of 'it'!!!")
+
+tooInsane = 'Math Blurfer'
+lords.remove(tooInsane)
+print(lords)
+input('Ahh, yes. Much better!')
+input(f"\nThe {tooInsane.title()} was banished many commits ago.")
+
+input("Anyways, we are inviting " + str(len(lords)) + " lords now.")
+input(lords)
+input("Well, that's UG-LEH! Let's sort it by alphabetical order.")
+input(sorted(lords))
+print("Nice. Bye!")
