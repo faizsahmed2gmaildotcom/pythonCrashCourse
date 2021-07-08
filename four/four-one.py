c947099ffb7269ec46e20ef16c4e3d50435548c5
faizs <faizsahmed@gmail.com>
2021-07-08 14:58:34 +0800

    Pizza.

---
+++ four/four-one.py
@@ -0,0 +1,6 @@
+pizza = ['volcano', 'veggie', 'chicken']
+pizzaLoc = 0
+for pizzaLen in range(len(pizza)):
+    input("I don't like " + pizza[pizzaLoc] + " pizza.")
+    pizzaLoc += 1
+input("I like pizza.")
--- three/three-seven.py
+++ three/three-seven.py
@@ -1,6 +1,6 @@
 invDinner = ['Burp', 'Tyee', 'Puu']
 dinNum = 0
-for dinNum in range (3):
+for dinNum in range(3):
     print('Dear ' + invDinner[dinNum] + ', please come for dinner.')
     dinNum += 1
 print("Hold up. Idiot Burp can't make it because he's too cool.")
@@ -8,7 +8,7 @@ del(invDinner[0])
 print("I'll just call upon... The Fart!")
 invDinner.append("Fart")
 dinNum = 0
-for dinNum in range (3):
+for dinNum in range(3):
     print('Dear ' + invDinner[dinNum] + ', please come for dinner.')
     dinNum += 1
 print("Wait, I found a bigger table. Let's add some more friends!")
@@ -20,10 +20,10 @@ for dinNum in range (6):
     dinNum += 1
 print("Great. Due to social distancing, I can only invite two people.")
 poppy = 0
-for poppy in range (4):
+for poppy in range(4):
     invDinner.pop
     poppy += 1
-for dinNum in range (2):
+for dinNum in range(2):
     print('Dear ' + invDinner[dinNum] + ', for the last time, please come for dinner.')
     dinNum += 1
 print("I'll just invite the rest later, after I fix this... SYSTEM BREACH, OH NONONONONONO... well, whatever. You "
