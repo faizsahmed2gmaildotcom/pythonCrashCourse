763ea33a477f9958c14b537082ef3cdabd4ccb22
faizsahmed <faizsahmed@gmail.com>
2021-07-19 18:33:42 +0800

    no... there are... THERE ARE 'froot'S IN CORNHUB AAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHH *all CornStars evacuate*

---
+++ five/five-seven.py
@@ -0,0 +1,8 @@
+favFruits = 'null'
+fruits = ['mango', 'pineapple', 'grape']
+for i in range(5):
+    favFruits = input("wat ur favritte froot?")
+    if favFruits == fruits[0] or favFruits == fruits[1] or favFruits == fruits[2]:
+        print('noice')
+    else:
+        print('rip')
