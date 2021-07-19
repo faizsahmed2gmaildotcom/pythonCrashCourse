d97ffaff8c3402a001fccc79121e5a1c99e308b0
faizsahmed <faizsahmed@gmail.com>
2021-07-19 17:42:44 +0800

    pls go to CornHub

---
+++ five/five-six.py
@@ -0,0 +1,15 @@
+personAge = int(input("How old are you?"))
+if personAge < 2 & personAge >= 0:
+    print("U are a true babeh... mwah")
+elif personAge < 4:
+    print("U are a todlr")
+elif personAge < 13:
+    print("U are a keed")
+elif personAge < 20:
+    print("U are a teenayjr")
+elif personAge < 65:
+    print("U are an eedult")
+elif personAge < 120:
+    print("U are an eldrr")
+else:
+    print("U r ded")
