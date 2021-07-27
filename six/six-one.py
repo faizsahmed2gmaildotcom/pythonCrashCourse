98f9223fbb7250377afb328fadfbbdf02b03f6fc
faizs <faizsahmed@gmail.com>
2021-07-27 15:26:37 +0800

    HO NOUW IMMA GONN GO DYE BAI aha, you're

--- six/six-one.py
+++ six/six-one.py
@@ -1,1 +1,4 @@
-person = {'firstName': 'Bob', 'lastName': 'Bill', 'city': 'Baskerville'}
+person = {'firstName': 'bob', 'lastName': 'bill', 'city': 'baskerville'}
+print(f"First name: {person['firstName'].title()}")
+print(f"Last name: {person['lastName'].title()}")
+print(f"City: {person['city'].title()}")
---
+++ six/six-three.py
@@ -0,0 +1,10 @@
+terms = {
+    'for loop': 'A loop that loops a desired amount of times by referring to an integer variable.',
+    'while loop': 'A loop that keeps on looping until a condition is met.',
+    'array': 'A list of items',
+    'KISS': 'Keep It Simple, Stupid!'
+}
+print(f"For Loop: {terms['for loop']}")
+print(f"While Loop: {terms['while loop']}")
+print(f"Array: {terms['array']}")
+print(f"KISS: {terms['KISS']} mwah")
