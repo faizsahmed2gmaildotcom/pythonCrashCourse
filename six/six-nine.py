ee64cf548ef17967267a2b410462f031f406aee2
faizs <faizsahmed@gmail.com>
2021-07-28 21:37:18 +0800

    That's a "gun" gun

--- six/six-nine.py
+++ six/six-nine.py
@@ -1,1 +1,25 @@
-sandlijandajnjncja h nama m.csnsa ns anb sdnbwf s ndcb ashc vzxb annbzn bd h  ascafdadamf  dsajn dasjk ndnja dnd jasd njs dasn dn hdhk bab b b b bas dbas dbad hw d
+favPlaces = {
+    "Bill": {
+        'name': 'Bill',
+        'placeOne': 'Bas',
+        'placeTwo': 'Oof',
+        'placeThree': 'Aaa',
+    },
+    "Bob": {
+        'name': 'Bob',
+        'placeOne': 'Kerville',
+        'placeTwo': 'Bulgaria',
+        'placeThree': 'poopoo',
+    },
+    "Jeff": {
+        'name': 'Jeff',
+        'placeOne': 'Place',
+        'placeTwo': 'Area',
+        'placeThree': 'City'
+    }
+}
+
+for favNumber, favInfo in favPlaces.items():
+    print(f"{favInfo['name']}'s Favourite Places: \n{favInfo['placeOne']}")
+    print(f"{favInfo['placeTwo']}")
+    print(f"{favInfo['placeThree']}\n")
