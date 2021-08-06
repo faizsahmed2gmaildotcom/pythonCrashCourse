20090a61397fb3c12d8b7f561e61288f2faa0394
faizs <faizsahmed@gmail.com>
2021-08-06 12:13:19 +0800

    poo

---
+++ eight/eight-seven.py
@@ -0,0 +1,11 @@
+def makeAlbum(artistName, albumTitle):
+    artist = {
+        'name': str(artistName),
+        'album': str(albumTitle),
+    },
+    for number, info in range(2):
+        print(f"Artist: {info['name'].title()}\nAlbum: {info['album'].title()}")
+
+nameInput = input("Artist name?")
+albumInput = input("Album title?")
+makeAlbum(str(nameInput), str(albumInput))
